60ef8df75b1daa0256080121ad0365fff4da115c
Ryusei1115 <tekunikaru22@gmail.com>
2023-05-10 23:33:05 +0900

    wip

--- Assets/Scripts/BulletBase.cs
+++ Assets/Scripts/BulletBase.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class BulletBase : IAbstractBullet
+public class BulletBase : MonoBehaviour, IAbstractBullet
 {
+    [SerializeField]
+    protected float _speed;
+
     public void Shoot()
     {
         
