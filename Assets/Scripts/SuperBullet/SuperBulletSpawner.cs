40f49c336c7b616ec9b22f8147cb5af1eec4fb14
Ryusei1115 <tekunikaru22@gmail.com>
2023-05-28 20:33:35 +0900

    wip

--- Assets/Scripts/SuperBullet/SuperBulletSpawner.cs
+++ Assets/Scripts/SuperBullet/SuperBulletSpawner.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalBulletSpawner : BulletSpawner
+public class SuperBulletSpawner : BulletSpawner
 {
     public override BaseSpawnBullet Spawn<T>(T param)
     {
-        _cache ??= Resources.Load<NormalBullet>(BulletPathManager.NORMAL_BULLET_PATH);
+        _cache ??= Resources.Load<SuperBullet>(BulletPathManager.SUPER_BULLET_PATH);
 
         var b = _cache;
         b.SetName(param.Name);
